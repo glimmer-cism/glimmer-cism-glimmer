9f4c73188d8d91c519480f2c58a395430cb41cc2
Magnus Hagdorn <Magnus.Hagdorn@ed.ac.uk>
2004-09-22 09:21:05 +0000

    This commit was manufactured by cvs2svn to create tag 'thk_mask_io'.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/tags/thk_mask_io@182 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- src/matlab/pmp.m
+++
@@ -1,3 +0,0 @@
-function a = pmp(t,z)
-
-a = t + 9.81 * 910 * 9.8e-8 * z;
--- src/matlab/tphead.m
+++
@@ -1,8 +0,0 @@
-function [time,str,ewn,nsn] = tpread(id);
-fread(id,1,'int');
-time = fread(id,1,'float');
-str = setstr(rot90(fread(id,4,'char')));
-ewn = fread(id,1,'int');
-nsn = fread(id,1,'int');
-dxy = fread(id,1,'float');
-fread(id,1,'int');
