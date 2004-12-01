ac7da191807f15222b48c9d03b87f06576b33539
Magnus Hagdorn <Magnus.Hagdorn@ed.ac.uk>
2004-12-01 13:08:41 +0000

    This commit was manufactured by cvs2svn to create tag 'eismint_1'.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/tags/eismint_1@226 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- src/matlab/blahhead.m
+++
@@ -1,9 +0,0 @@
-function [time,varb,flag,scal] = blahhead(id)
-
-% this functions reads data header from 3d file
-
-% read header which is time stamp; variable code; grid-type code; scaling
- 
-fread(id,1,'int');
-time = fread(id,1,'float'); varb = fread(id,1,'int'); flag = fread(id,1,'int'); scal = fread(id,1,'float'); 
-fread(id,1,'int');
--- src/matlab/tpsurf.m
+++
@@ -1,9 +0,0 @@
-function tpsurf(x,z,a,dim,num)
-
-if dim == 3
-  surf(x(:,num),tpsq(z,dim,num),tpsq(a,dim,num));
-elseif dim ==2
-  surf(x(num,:),tpsq(z,dim,num),tpsq(a,dim,num));
-end
-
-view(2); axis tight; colorbar
