8807370cf746b23d3c5f406ffe5e3ef7dbff9afc
Ian Rutt <i.c.rutt@swansea.ac.uk>
2005-01-07 15:28:00 +0000

    Began process of making it possible to use the Energy balance mass balance model with Glimmer
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/trunk@251 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

---
+++ src/fortran/glide_enmabal.f90
@@ -0,0 +1,14 @@
+
+module glide_enmabal
+
+  !*FD The energy and mass-balance model
+
+contains
+
+	subroutine enmabal
+
+    !*FD Dummy subroutine, called from calcacab
+
+  end subroutine enmabal
+
+end module glide_enmabal
