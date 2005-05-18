70aaeb7e0a6edaad0ce057ac39b1a9c04f142b35
Ian Rutt <i.c.rutt@swansea.ac.uk>
2005-05-18 17:03:59 +0000

    Changes to mass-balance coupling for ENMABAL; also enlarged mass-balance io
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/trunk@461 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- src/fortran/glint_mbal_coupling.f90
+++ src/fortran/glint_mbal_coupling.f90
@@ -14,6 +14,16 @@ module glint_mbal_coupling
     real(sp),dimension(:,:),pointer :: artm_save => null() !*FD used to average air-temperature
     real(sp),dimension(:,:),pointer :: snowd     => null() !*FD Keeps track of snow depth
     real(sp),dimension(:,:),pointer :: siced     => null() !*FD Keeps track of superimposed ice depth 
+    real(sp),dimension(:,:),pointer :: prcp      => null() !*FD Instantaneous precip
+    real(sp),dimension(:,:),pointer :: ablt      => null() !*FD Instantaneous ablation
+    real(sp),dimension(:,:),pointer :: acab      => null() !*FD Instantaneous mass-balance
+    real(sp),dimension(:,:),pointer :: artm      => null() !*FD Instantaneous air temperature
+    real(sp),dimension(:,:),pointer :: xwind      => null() !*FD Instantaneous x-wind
+    real(sp),dimension(:,:),pointer :: ywind      => null() !*FD Instantaneous y-wind
+    real(sp),dimension(:,:),pointer :: humidity   => null() !*FD Instantaneous humidity
+    real(sp),dimension(:,:),pointer :: SWdown     => null() !*FD Instantaneous sw down
+    real(sp),dimension(:,:),pointer :: LWdown     => null() !*FD Instantaneous lw down
+    real(sp),dimension(:,:),pointer :: Psurf      => null() !*FD Instantaneous psurf
     real(sp),dimension(:,:),pointer :: snowd_save => null() !*FD Saves snow depth
     real(sp),dimension(:,:),pointer :: siced_save => null() !*FD Saves superimposed ice depth 
     integer :: av_count =0 !*FD Counter for averaging temperature input
@@ -41,6 +51,16 @@ contains
     if (associated(params%artm_save))  deallocate(params%artm_save)
     if (associated(params%snowd))      deallocate(params%snowd)
     if (associated(params%siced))      deallocate(params%siced)
+    if (associated(params%prcp))       deallocate(params%prcp)
+    if (associated(params%ablt))       deallocate(params%ablt)
+    if (associated(params%acab))       deallocate(params%acab)
+    if (associated(params%artm))       deallocate(params%artm)
+    if (associated(params%xwind))      deallocate(params%xwind)
+    if (associated(params%ywind))      deallocate(params%ywind)
+    if (associated(params%humidity))   deallocate(params%humidity)
+    if (associated(params%SWdown))     deallocate(params%SWdown)
+    if (associated(params%LWdown))     deallocate(params%LWdown)
+    if (associated(params%Psurf))      deallocate(params%Psurf)
     if (associated(params%snowd_save)) deallocate(params%snowd_save)
     if (associated(params%siced_save)) deallocate(params%siced_save)
 
@@ -52,6 +72,16 @@ contains
     allocate(params%artm_save(proj%nx,proj%ny));  params%artm_save = 0.0
     allocate(params%snowd(proj%nx,proj%ny));      params%snowd = 0.0
     allocate(params%siced(proj%nx,proj%ny));      params%siced = 0.0
+    allocate(params%prcp(proj%nx,proj%ny));       params%prcp = 0.0
+    allocate(params%ablt(proj%nx,proj%ny));       params%ablt = 0.0
+    allocate(params%acab(proj%nx,proj%ny));       params%acab = 0.0
+    allocate(params%artm(proj%nx,proj%ny));       params%artm = 0.0
+    allocate(params%xwind(proj%nx,proj%ny));      params%xwind = 0.0
+    allocate(params%ywind(proj%nx,proj%ny));      params%ywind = 0.0
+    allocate(params%humidity(proj%nx,proj%ny));   params%humidity = 0.0
+    allocate(params%SWdown(proj%nx,proj%ny));     params%SWdown = 0.0
+    allocate(params%LWdown(proj%nx,proj%ny));     params%LWdown = 0.0
+    allocate(params%Psurf(proj%nx,proj%ny));      params%Psurf = 0.0
     allocate(params%snowd_save(proj%nx,proj%ny)); params%snowd_save = 0.0
     allocate(params%siced_save(proj%nx,proj%ny)); params%siced_save = 0.0
 
@@ -118,6 +148,19 @@ contains
     params%ablt_save = params%ablt_save + ablt
     params%acab_save = params%acab_save + acab
     params%artm_save = params%artm_save + artm
+
+    ! Copy instantaneous fields
+
+    params%prcp=prcp
+    params%ablt=ablt
+    params%acab=acab
+    params%artm=artm
+    params%xwind=xwind
+    params%ywind=ywind
+    params%humidity=humidity
+    params%SWdown=SWdown
+    params%LWdown=LWdown
+    params%Psurf=Psurf
 
   end subroutine glint_accumulate
 
