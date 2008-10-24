f543b7fe34e00930a636a8af4fdd4c870f54f438
Tim Bocek <tim.bocek@gmail.com>
2008-10-24 21:13:19 +0000

    Added Glimmer's simple example driver to the repo, as this is a nice sanity check
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@852 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

---
+++ example/example.f90
@@ -0,0 +1,94 @@
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN"
+ "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<title>CVS log for glimmer-example/example.f90</title>
+<!-- hennerik CVSweb $Revision: 1.112 $ -->
+</head>
+<body text="#000000" bgcolor="#ffffff">
+ <h1 align="center">CVS log for glimmer-example/example.f90</h1>
+<A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/?cvsroot=glimmer#example.f90"><IMG SRC="/icons/cvsweb/back.gif" ALT="[BACK]" BORDER="0" WIDTH="20" HEIGHT="22"></A>
+ <b>Up to  <a href="/cgi-bin/cvsweb.cgi/?cvsroot=glimmer#dirlist">[glimmer]</a> / <a href="/cgi-bin/cvsweb.cgi/glimmer-example/?cvsroot=glimmer#dirlist">glimmer-example</a></b><p>
+<A HREF="#diff">Request diff between arbitrary revisions</A>
+<HR NOSHADE>
+Default branch: 1<BR>
+<HR size=1 NOSHADE><a NAME="rev1.5"></a><a NAME="RELEASE_0_4"></a><a NAME="HEAD"></a>
+ Revision <A HREF="example.f90?rev=1.5&amp;content-type=text/x-cvsweb-markup&amp;cvsroot=glimmer"><b>1.5</b></A> / (<A HREF="/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.5&amp;content-type=text/plain&amp;cvsroot=glimmer" target="cvs_checkout" onClick="window.open('/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.5&amp;content-type=text/plain','cvs_checkout','resizeable,scrollbars');"><b>download</b></A>) - <a href="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?annotate=1.5&amp;cvsroot=glimmer">annotate</a> - <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?r1=1.5&amp;cvsroot=glimmer">[select for diffs]</A>
+, <i>Mon Oct  3 13:48:31 2005 UTC</i> (3 years ago) by <i>magi</i>
+<BR>CVS Tags: <b><A HREF="example.f90?cvsroot=glimmer&amp;only_with_tag=RELEASE_0_4">RELEASE_0_4</A>,
+<A HREF="example.f90?cvsroot=glimmer&amp;only_with_tag=HEAD">HEAD</A>
+</b><BR>Changes since <b>1.4: +3 -2
+ lines</b><BR>Diff to previous <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90.diff?r1=1.4&amp;r2=1.5&amp;cvsroot=glimmer">1.4</A>
+<PRE>
+use new glide API
+</PRE>
+<HR size=1 NOSHADE><a NAME="rev1.4"></a><a NAME="RELEASE_0_3"></a>
+ Revision <A HREF="example.f90?rev=1.4&amp;content-type=text/x-cvsweb-markup&amp;cvsroot=glimmer"><b>1.4</b></A> / (<A HREF="/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.4&amp;content-type=text/plain&amp;cvsroot=glimmer" target="cvs_checkout" onClick="window.open('/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.4&amp;content-type=text/plain','cvs_checkout','resizeable,scrollbars');"><b>download</b></A>) - <a href="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?annotate=1.4&amp;cvsroot=glimmer">annotate</a> - <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?r1=1.4&amp;cvsroot=glimmer">[select for diffs]</A>
+, <i>Mon Feb 28 16:43:02 2005 UTC</i> (3 years, 7 months ago) by <i>magi</i>
+<BR>CVS Tags: <b><A HREF="example.f90?cvsroot=glimmer&amp;only_with_tag=RELEASE_0_3">RELEASE_0_3</A>
+</b><BR>Changes since <b>1.3: +1 -0
+ lines</b><BR>Diff to previous <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90.diff?r1=1.3&amp;r2=1.4&amp;cvsroot=glimmer">1.3</A>
+<PRE>
+rule to build distribution
+</PRE>
+<HR size=1 NOSHADE><a NAME="rev1.3"></a>
+ Revision <A HREF="example.f90?rev=1.3&amp;content-type=text/x-cvsweb-markup&amp;cvsroot=glimmer"><b>1.3</b></A> / (<A HREF="/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.3&amp;content-type=text/plain&amp;cvsroot=glimmer" target="cvs_checkout" onClick="window.open('/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.3&amp;content-type=text/plain','cvs_checkout','resizeable,scrollbars');"><b>download</b></A>) - <a href="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?annotate=1.3&amp;cvsroot=glimmer">annotate</a> - <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?r1=1.3&amp;cvsroot=glimmer">[select for diffs]</A>
+, <i>Wed Feb 16 16:43:23 2005 UTC</i> (3 years, 8 months ago) by <i>magi</i>
+<BR>Changes since <b>1.2: +25 -14
+ lines</b><BR>Diff to previous <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90.diff?r1=1.2&amp;r2=1.3&amp;cvsroot=glimmer">1.2</A>
+<PRE>
+use accessor functions
+</PRE>
+<HR size=1 NOSHADE><a NAME="rev1.2"></a>
+ Revision <A HREF="example.f90?rev=1.2&amp;content-type=text/x-cvsweb-markup&amp;cvsroot=glimmer"><b>1.2</b></A> / (<A HREF="/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.2&amp;content-type=text/plain&amp;cvsroot=glimmer" target="cvs_checkout" onClick="window.open('/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.2&amp;content-type=text/plain','cvs_checkout','resizeable,scrollbars');"><b>download</b></A>) - <a href="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?annotate=1.2&amp;cvsroot=glimmer">annotate</a> - <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?r1=1.2&amp;cvsroot=glimmer">[select for diffs]</A>
+, <i>Tue Feb  8 13:46:30 2005 UTC</i> (3 years, 8 months ago) by <i>magi</i>
+<BR>Changes since <b>1.1: +8 -4
+ lines</b><BR>Diff to previous <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90.diff?r1=1.1&amp;r2=1.2&amp;cvsroot=glimmer">1.1</A>
+<PRE>
+forgot scaling...
+</PRE>
+<HR size=1 NOSHADE><a NAME="rev1.1"></a>
+ Revision <A HREF="example.f90?rev=1.1&amp;content-type=text/x-cvsweb-markup&amp;cvsroot=glimmer"><b>1.1</b></A> / (<A HREF="/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.1&amp;content-type=text/plain&amp;cvsroot=glimmer" target="cvs_checkout" onClick="window.open('/cgi-bin/cvsweb.cgi/~checkout~/glimmer-example/example.f90?rev=1.1&amp;content-type=text/plain','cvs_checkout','resizeable,scrollbars');"><b>download</b></A>) - <a href="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?annotate=1.1&amp;cvsroot=glimmer">annotate</a> - <A HREF="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90?r1=1.1&amp;cvsroot=glimmer">[select for diffs]</A>
+, <i>Thu Feb  3 11:21:32 2005 UTC</i> (3 years, 8 months ago) by <i>magi</i>
+<BR>Diff<PRE>
+initial version
+</PRE>
+<HR NOSHADE><A NAME=diff>
+This form allows you to request diff's between any two
+revisions of a file.  You may select a symbolic revision
+name using the selection box or you may type in a numeric
+name using the type-in text box.
+</A><P>
+<FORM METHOD="GET" ACTION="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90.diff" NAME="diff_select">
+<INPUT TYPE=HIDDEN NAME="cvsroot" VALUE="glimmer">
+Diffs between 
+<SELECT NAME="r1">
+<OPTION VALUE="text" SELECTED>Use Text Field
+<OPTION VALUE="1.5:RELEASE_0_4">RELEASE_0_4
+<OPTION VALUE="1.4:RELEASE_0_3">RELEASE_0_3
+<OPTION VALUE="0.1:MAIN">MAIN
+<OPTION VALUE="1.5:HEAD">HEAD
+</SELECT>
+<INPUT TYPE="TEXT" SIZE="12" NAME="tr1" VALUE="1.1" onChange='document.diff_select.r1.selectedIndex=0'>
+ and 
+<SELECT NAME="r2">
+<OPTION VALUE="text" SELECTED>Use Text Field
+<OPTION VALUE="1.5:RELEASE_0_4">RELEASE_0_4
+<OPTION VALUE="1.4:RELEASE_0_3">RELEASE_0_3
+<OPTION VALUE="0.1:MAIN">MAIN
+<OPTION VALUE="1.5:HEAD">HEAD
+</SELECT>
+<INPUT TYPE="TEXT" SIZE="12" NAME="tr2" VALUE="1.5" onChange='document.diff_select.r2.selectedIndex=0'>
+<BR>Type of Diff should be a&nbsp;<SELECT NAME="f">
+<OPTION VALUE=h SELECTED>Colored Diff<OPTION VALUE=H>Long Colored Diff<OPTION VALUE=u>Unidiff<OPTION VALUE=c>Context Diff</SELECT><INPUT TYPE=SUBMIT VALUE="  Get Diffs  ">
+</FORM>
+<HR noshade>
+<A name=logsort></A>
+<FORM METHOD="GET" ACTION="/cgi-bin/cvsweb.cgi/glimmer-example/example.f90">
+<INPUT TYPE=HIDDEN NAME="cvsroot" VALUE="glimmer">
+Sort log by: 
+<SELECT NAME="logsort" onchange="submit()">
+<OPTION VALUE=cvs>Not sorted<OPTION VALUE=date SELECTED>Commit date<OPTION VALUE=rev>Revision</SELECT>
+<INPUT TYPE=SUBMIT VALUE="  Sort  ">
+</FORM>
+<hr noshade>Back to <a href="/">forge.nesc.ac.uk</a> / <a href="/projects/glimmer">glimmer</a></body></html>
+</BODY></HTML>
