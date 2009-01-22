f134ea031beffb0f34df788417fb0ffd2cb81f9b
Tim Bocek <tim.bocek@gmail.com>
2009-01-22 18:25:04 +0000

    Finished ISMIP-HOM F experiment.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@902 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- tests/ISMIP-HOM/ismip_hom_f.py
+++ tests/ISMIP-HOM/ismip_hom_f.py
@@ -4,7 +4,7 @@ import numpy
 import pylab
 import glimcdf
 
-#Create a vector of (x,y,z) coordinates the represent the coordinates of each point generated for the 
+#Create a vector of (x,y,bed,thk) coordinates the represent the coordinates of each point generated for the 
 #bed of ismip-hom f.  This is a funky way to do it, but we do it this way because additional
 #transformation will be needed before these are ready to use
 def createBumpPoints(nx, ny, H0=1000):
@@ -22,11 +22,11 @@ def createBumpPoints(nx, ny, H0=1000):
     points = []
 
     for i in range(nx):
-        for j in range(ny/2, ny/2+1):
+        for j in range(ny):
             x = float(minX + deltaX*i)
             y = float(minY + deltaY*j)
             z = -H0 + a0*( math.exp( -(x**2 + y**2) / (sigma**2) ) )
-
+            thk = -z
             points.append((x,y,z))
 
     return points
@@ -34,6 +34,7 @@ def createBumpPoints(nx, ny, H0=1000):
 #Rotates a list of (x,y,z) points by the given angle about the y axis (so that all the slope is in
 #the x axis and none in the y)
 def rotateBumpPoints(points, theta=.3):
+
     outputPoints = []
     for point in points:
         x,y,z = point
@@ -56,39 +57,47 @@ def collectYCoordinateLines(points):
         if y not in collectedLines:
             collectedLines[y] = []
         collectedLines[y].append(point)
-        print collectedLines
     return collectedLines
 
 #Sorts an array of (x,y,z) corrdinates by the x value and returns a new sorted array
 def sortCoordinateLine(points):
     return sorted( points, lambda l1, l2:cmp(l1[0], l2[0]) )
 
-def interpolateCoordinateLine(points, startX, deltaX):
-    if points[0][0] > startX:
-        raise Exception("The starting x is before the first x provided (you are asking for extrapolation, not interpolation)")
+def interpolateCoordinateLine(points, oldPoints):
+    extrapolationSlope = (points[1][2] - points[0][2]) / (points[1][0] - points[0][0])
+    initialX = points[0][0]
+    initialZ = points[0][2]
 
+    finalX = points[len(points) - 1][0]
+    finalZ = points[len(points) - 1][2]
+
+    i = 0
     newPoints = []
-    x = startX
-    maxX = points[len(points) - 1][0]
 
-    xzPlot(points)
+    currentInterpIndex = 0
 
-    currentOldPoint = 0
-    while x < maxX:
-        #See if we need to consider the next interval.  This happens if the x value we are interpolating onto
-        #is greater than the end point of the current interval
-        while points[currentOldPoint + 1][0] < x:
-            currentOldPoint += 1
-        
-        p1 = points[currentOldPoint]
-        p2 = points[currentOldPoint + 1]
-        
-        print p1[0],x,p2[0]
-        
-        alpha = (x - p1[0])/(p2[0] - p1[0])
-        z = p1[2]*alpha + p2[2]*(1-alpha)
-        newPoints.append((x, p1[1], z))
-        x += deltaX
+    #Extrapolate with a sloped line up until the first place where we have data.
+    for x,y,z in oldPoints:
+        #Extrapolate with a sloped line up until the first place where we have data.
+        if x < initialX:
+            zprime = initialZ - extrapolationSlope*(initialX - x)
+            #zprime = 0
+        elif x > finalX:
+            zprime = finalZ   + extrapolationSlope*(x - finalX)
+            #zprime = 0
+        else: #Interpolate
+            #Because these points are ordered by x coordinate in both lists, we can be sure that
+            #the starting point for the interpolation will never be *before* the starting point
+            #of the last interpolation.  This cuts our searching down to an average of constant time
+            #for the whole run.
+            while points[currentInterpIndex + 1][0] < x:
+                currentInterpIndex += 1
+            #We stopped at the first new point that is after the x value we want.  Therefore, the x value
+            #we want falls between the two indices we've identified
+            alpha = (x - points[currentInterpIndex][0]) / (points[currentInterpIndex+1][0] - points[currentInterpIndex][0])
+            
+            zprime = alpha*points[currentInterpIndex+1][2] + (1-alpha)*points[currentInterpIndex][2]
+        newPoints.append((x,y,zprime))
     return newPoints
 
 def xzPlot(points):
@@ -103,29 +112,41 @@ if __name__ == "__main__":
 
     nc, shape = glimcdf.nc_from_config("ishom.f.config")
     topg = glimcdf.setup_variable(nc, "topg")
+    thk  = glimcdf.setup_variable(nc, "thk" )
+
     nx = shape.nx
     ny = shape.ny
 
     points = createBumpPoints(nx, ny)
-    points = rotateBumpPoints(points, 3*math.pi/180) #Rotate points so average slope is 3 degrees
 
     lineDict = collectYCoordinateLines(points)
     lines = sorted(list(lineDict.items()), lambda l1, l2:cmp(l1[0], l2[0]))
 
     j = 0
 
-    output = numpy.zeros((nx, ny))
-
+    topg_field = numpy.zeros((nx, ny))
+    thck_field = numpy.zeros((nx, ny))
+    
     for y, points in lines:
-        points = sortCoordinateLine(points)
-        points = interpolateCoordinateLine(points, 0, 1000*100/nx)
-        for i, point in enumerate(points):
-            output[i,j] = point[2]
+        newpoints = rotateBumpPoints(points,3*math.pi/180)
+        newpoints = sortCoordinateLine(newpoints)
+        newpoints = interpolateCoordinateLine(newpoints, points)
+        
+        #Get a point 1000 meters above the first point to represent the (smooth) surface
+        #elevation.  We'll keep adding a constant slope to that to give us the surface, which
+        #in turn easily gives us the thickness.
+        currentSurf = 1000 + newpoints[0][2]
+        slope = 1000 * math.tan(3.0*math.pi/180)
+
+        for i, point in enumerate(newpoints):
+            topg_field[i,j] = point[2]
+            thck_field[i,j] = currentSurf - point[2]
+            currentSurf += slope
 
         j += 1
 
     for i in range(nx):
         for j in range(ny):
-            topg.put_1((0,i,j), output[i,j])
+            topg.put_1((0,i,j), topg_field[i,j])
+            thk.put_1( (0,i,j), thck_field[i,j])
 
-    print output
