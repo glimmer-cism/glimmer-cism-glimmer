ef1b6575c8c1280fdc67746b4ee987ac69da9cfe
Tim Bocek <tim.bocek@gmail.com>
2009-01-23 23:03:48 +0000

    Created python script that gets the netcdf output from ISMIP-HOM runs into the format required by the ISMIP-HOM document.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@911 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

---
+++ tests/ISMIP-HOM/formatData.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+#usage: formatData.py experiment_letter infilename outfilename
+
+#This module includes helper routines for use in ISMIP-HOM verification.  In particular, 
+#the contained routines are used to extract data for intercomparison
+
+import sys
+import pycdf
+
+def extractMapView(ncdf, fields, fromSurface):
+    #Get the staggered grid dimensions
+    x0dim = ncdf.dim("x0")
+    y0dim = ncdf.dim("y0")
+    nx = x0dim.inq_len()
+    ny = y0dim.inq_len()
+
+    vars = [ncdf.var(f) for f in fields]
+
+
+    rows = []
+    time = 0
+
+    for i in range(nx):
+        for j in range(ny):
+            xhat = float(i)/float(nx - 1)
+            yhat = float(j)/float(ny - 1)
+
+            row = [xhat, yhat]
+            for var, s in zip(vars, fromSurface):
+                #Get the level that we are extracting from
+                if s:
+                    level = 0
+                else: #from base
+                    level = ncdf.dim("level").inq_len() - 1
+
+                row.append(var.get_1((time,level,j,i)))
+                
+            rows.append(row)
+
+    return rows
+
+def extractFlowline(ncdf, fields, fromSurface, transpose=False):
+    #Get the staggered grid dimensions
+    x0dim = ncdf.dim("x0")
+    y0dim = ncdf.dim("y0")
+    
+    nx = x0dim.inq_len()
+    ny = y0dim.inq_len()
+
+    if transpose:
+        nPoints = ny
+        flowlineCenter = nx/2
+    else:
+        nPoints = nx
+        flowlineCenter = ny/2
+
+    vars = [ncdf.var(f) for f in fields]
+
+    rows = []
+    time = 0
+
+    for i in range(nPoints):
+        xhat = float(i)/float(nPoints - 1)
+
+        row = [xhat]
+        for var,s in zip(vars,fromSurface):
+            #Get the level that we are extracting from
+            if s:
+                level = 0
+            else: #from base
+                level = ncdf.dim("level").inq_len() - 1
+
+            if transpose:
+                row.append(var.get_1((time,level,i,flowlineCenter)))
+            else:
+                row.append(var.get_1((time,level,flowlineCenter,i)))
+        rows.append(row)
+
+    return rows
+
+if __name__ == "__main__":
+    experimentLetter = sys.argv[1].lower()
+    inFileName = sys.argv[2]
+    outFileName = sys.argv[3]
+
+    ncdf = pycdf.CDF(inFileName)
+
+    if experimentLetter == "a":
+        rows = extractMapView(ncdf, ["uvelhom","vvelhom","wvel","tau_xz","tau_yz"],
+                                    [True,     True,     True,  False,   False])
+    elif experimentLetter == "b":
+        rows = extractFlowline(ncdf, ["uvelhom", "wvel", "tau_xz"],
+                                     [True,      True,   False])
+    elif experimentLetter == "c":
+        rows = extractMapView(ncdf, ["uvelhom","vvelhom","wvel","uvelhom","vvelhom","tau_xz","tau_yz"],
+                                    [True,     True,     True,  False,    False,    False,   False])
+    elif experimentLetter == "d":
+        rows = extractFlowline(ncdf, ["uvelhom", "wvel", "uvelhom", "tau_xz"],
+                                     [True,      True,   False,     false])
+
+    ncdf.close()
+
+    #Output
+    outfile = open(outFileName, "w")
+    for line in rows:
+        print >>outfile, "\t".join([str(i) for i in line])
+    outfile.close()
