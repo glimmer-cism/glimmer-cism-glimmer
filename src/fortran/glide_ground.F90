947abd5885c627b53b1a5d693664140d69723ab9
Tim Wylie <twylie@users.berlios.de>
2009-04-16 23:16:05 +0000

    Added some grounding line functions for subgrid tracking.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@999 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- src/fortran/glide_ground.F90
+++ src/fortran/glide_ground.F90
@@ -13,12 +13,11 @@
 #include <glide_mask.inc>
 module glide_ground
   use glide_types
-  use glimmer_log  !do i need this?
   use glimmer_global
   implicit none
 contains
+!-------------------------------------------------------------------------
   subroutine glide_initialise_backstress(thck,backstress)
-     use glimmer_global, only : dp,sp
      implicit none
      
      real(dp), dimension(:,:), intent(in) :: thck !*FD Ice thickness
@@ -156,5 +155,180 @@ contains
     end select
 
   end subroutine glide_marinlim
-
+  
+!-------------------------------------------------------------------------
+  !This function returns the correct grounding line using the data given 
+  ! the mask reference point.  dir is specifying 'ew' or 'ns', but can be 
+  ! left null if there's only one option.
+  real function get_ground_line(model,ns,ew,dir)
+     use glide_types
+     implicit none
+     type(glide_global_type) :: model        !*FD model instance
+     integer ns,ew !grounding line in ns/ew direction
+     character(len=1) dir !dir - specifies ns/ew
+     real appr_ground !grounding line
+     !this is assuming a greedy grounding line progression.  Always setting the
+     ! mask by rounding up to the next grid point
+     if (dir == "s") then
+        !look above/below for non-null value
+        appr_ground = model%ground%gl_ns(ns-1,ew)
+     else if (dir == "e") then
+        appr_ground = model%ground%gl_ew(ns,ew-1)
+     else if (dir == "n") then
+        appr_ground = model%ground%gl_ns(ns,ew)
+     else if (dir == "w") then
+        appr_ground = model%ground%gl_ew(ns,ew)
+     end if
+     return
+  end function get_ground_line
+    
+!-------------------------------------------------------------------------
+  subroutine set_ground_line(model,ns,ew,dir,value)
+     use glide_types
+     implicit none
+     type(glide_global_type) :: model        !*FD model instance
+     integer, intent(in) :: ns !grounding line in ns direction
+     integer, intent(in) :: ew !grounding line in ew direction
+     real(sp), intent(in) :: value !grounding line in ew direction
+     character(len=1), intent(in) :: dir !dir - specifies ns/ew
+     !this is assuming a greedy grounding line progression.  Always setting the
+     ! mask by rounding up to the next grid point
+     if (dir == "s") then
+        model%ground%gl_ns(ns-1,ew) = value
+     else if (dir == "e") then
+        model%ground%gl_ew(ns,ew-1) = value
+     else if (dir == "n") then
+        model%ground%gl_ns(ns,ew) = value
+     else if (dir == "w") then
+        model%ground%gl_ew(ns,ew) = value
+     end if
+  end subroutine set_ground_line
+!-------------------------------------------------------------------------
+  real function lin_reg_xg(model,ns,ew,j1ns,j1ew,direction)
+     use glide_types
+     use glimmer_physcon, only : rhoi, rhoo
+     type(glide_global_type) :: model        !*FD model instance
+     integer, intent(in) :: ns !grounding line in ns direction
+     integer, intent(in) :: ew !grounding line in ew direction
+     integer, intent(in) :: j1ns !ice shelf in ns direction
+     integer, intent(in) :: j1ew !ice shelf line in ew direction
+     character(len=1), intent(in) :: direction         !direction the grnd line is facing
+     real(sp) ::  xg                     !grounding line
+     real(sp) ::  dx                      !distance between gridpts
+     real(sp) ::  xj                        !grounding line
+     real(sp) :: fj                        !f at grid pnt j
+     real(sp) :: fj_1                      !f evaluated at j (+/-) 1
+     real(sp) :: df                        !delta f of fj,jf_1
+     if (direction .eq. "s" .or. direction .eq. "n") then
+        dx = model%numerics%dns
+        xj = ns*dx
+     else
+        dx = model%numerics%dew
+        xj = ew*dx
+     end if
+     !set the pattyn f function - assuming ocean water
+     fj = (model%climate%eus - model%geometry%topg(ew,ns))*rhoo/(rhoi*model%geometry%thck(ew,ns))
+     fj_1 = (model%climate%eus - model%geometry%topg(j1ew,j1ns))*rhoo/(rhoi*model%geometry%thck(j1ew,j1ns))
+     df = (fj_1 - fj)/dx                
+     xg = (1 - fj + df*xj)/df
+     return 
+  end function lin_reg_xg
+!-------------------------------------------------------------------------
+  !also need an update mask
+  subroutine update_ground_line(model,mask)
+     use glide_mask
+     implicit none
+     type(glide_global_type) :: model        !*FD model instance
+     integer, dimension(:,:),pointer :: mask    !*FD grid type mask
+     integer ew,ns,jns,jew,j1ns,j1ew
+     character(len=1) :: direction         !direction the grnd line is facing
+     real(sp) :: xg                        !grounding line
+     !this is assuming a greedy grounding line progression.  Always setting the
+     ! mask by rounding up to the next grid point so the grounding line is on floating ice/ocean
+     do ns = 1,model%general%nsn
+        do ew = 1,model%general%ewn
+            if (GLIDE_IS_GROUNDING_LINE(mask(ew,ns))) then
+                call set_ground_line(model,ns,ew,"n",0.0)
+                call set_ground_line(model,ns,ew,"s",0.0)
+                call set_ground_line(model,ns,ew,"e",0.0)
+                call set_ground_line(model,ns,ew,"w",0.0)
+                !since the grounding line always rounds up, we check behind it
+                if (GLIDE_IS_OCEAN(mask(ew,ns)) .or. GLIDE_IS_FLOAT(mask(ew,ns))) then
+                    !staying or retreating
+                    !southern grounding line
+                    if (.not. (GLIDE_IS_OCEAN(mask(ew,ns - 1)) &
+                            .or. (GLIDE_IS_FLOAT(mask(ew,ns - 1))))) then
+                        xg = lin_reg_xg(model,ns-1,ew,ns,ew,"s")
+                        call set_ground_line(model,ns-1,ew,"s",xg)
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew,ns - 2)) &
+                            .or. (GLIDE_IS_FLOAT(mask(ew,ns - 2))))) then
+                        xg = lin_reg_xg(model,ns-2,ew,ns-1,ew,"s")
+                        call set_ground_line(model,ns-2,ew,"s",xg)  !should this be ns-1
+                        !update mask 
+                    !northern grounding line    
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew,ns + 1)) & 
+                            .or. GLIDE_IS_FLOAT(mask(ew,ns + 1)))) then
+                        xg = lin_reg_xg(model,ns+1,ew,ns,ew,"n")
+                        call set_ground_line(model,ns+1,ew,"n",xg)
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew,ns + 2)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew,ns + 2)))) then
+                        xg = lin_reg_xg(model,ns+2,ew,ns+1,ew,"n")
+                        call set_ground_line(model,ns-2,ew,"n",xg)
+                    end if
+                else
+                    !advancing
+                    !southern grounding line
+                    if (.not. (GLIDE_IS_OCEAN(mask(ew,ns + 1)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew,ns + 1)))) then
+                        xg = lin_reg_xg(model,ns+1,ew,ns,ew,"s")
+                        call set_ground_line(model,ns+1,ew,"s",xg)
+                    !northern grounding line    
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew,ns - 1)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew,ns - 1)))) then
+                        xg = lin_reg_xg(model,ns-1,ew,ns,ew,"n")
+                        call set_ground_line(model,ns-1,ew,"n",xg)
+                    end if
+                end if
+                
+                if (GLIDE_IS_OCEAN(mask(ew,ns)) .or. GLIDE_IS_FLOAT(mask(ew,ns))) then
+                    !staying or retreating
+                    !western grounding line
+                    if (.not. (GLIDE_IS_OCEAN(mask(ew + 1,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew + 1,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew + 1,ns,ew,"w")
+                        call set_ground_line(model,ns,ew+1,"w",xg)
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew + 2,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew + 2,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew + 2,ns,ew + 1,"w")
+                        call set_ground_line(model,ns,ew+2,"w",xg)
+                    !eastern grounding line    
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew - 1,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew - 1,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew - 1,ns,ew,"e")
+                        call set_ground_line(model,ns,ew-1,"e",xg)
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew - 2,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew - 2,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew - 2,ns,ew - 1,"e")
+                        call set_ground_line(model,ns,ew-2,"e",xg)
+                    end if
+                else
+                    !advancing
+                    !western grounding line
+                    if (.not. (GLIDE_IS_OCEAN(mask(ew-1,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew-1,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew-1,ns,ew,"e")
+                        call set_ground_line(model,ns,ew-1,"e",xg)
+                    !eastern grounding line    
+                    else if (.not. (GLIDE_IS_OCEAN(mask(ew+1,ns)) &
+                            .or. GLIDE_IS_FLOAT(mask(ew+1,ns)))) then
+                        xg = lin_reg_xg(model,ns,ew+1,ns,ew,"w")
+                        call set_ground_line(model,ns,ew+1,"w",xg)
+                    end if
+                end if
+                !need to update mask
+            end if 
+        end do
+     end do
+  end subroutine update_ground_line
+!---------------------------------------------------------------------------
 end module glide_ground
