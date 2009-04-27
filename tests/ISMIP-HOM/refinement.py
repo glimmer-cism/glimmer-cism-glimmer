9ec3765d0270aaee6be319862b7062d2686e2f85
Tim Bocek <tim.bocek@gmail.com>
2009-04-27 00:56:23 +0000

    Code to do a more intelligent ISMIP-HOM refinement experiment set
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@1028 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- tests/ISMIP-HOM/refinement.py
+++ tests/ISMIP-HOM/refinement.py
@@ -1,34 +1,76 @@
 #!/usr/bin/python
+#The purpose of this script is to run the ISMIP-HOM suite for various grid refinements and solver options
+#to study their impact on the model output.
 import os
 
 k = 10
 guideFile = open("refinement-experiments.txt","w")
-for gridSizeArg in ["20","40","80"]:
-    for vertGridArg in ["20","40","80"]:
-        for vertGridSpacing in ["glimmer","pattyn","even"]:
-            for diagnosticScheme, strDiagnostic in [("1","unstaggered"),("2","staggered")]:
-                
-                print >>guideFile, k, gridSizeArg, vertGridArg, vertGridSpacing, strDiagnostic
-                prefix = "gl"+str(k)
+
+def runExperiment(gridSizeArg="40", vertGridArg="40", vertGridSpacing="even", diagnosticScheme="2"):
+    global k
+    global guideFile
+
+    gridSizeArg= str(gridSizeArg)
+    vertGridArg= str(vertGridArg)
+    diagnosticScheme= str(diagnosticScheme)
+
+    strDiagnostic = {"1":"unstaggered". "2":"staggered"}[diagnosticScheme]
+
+    print >>guideFile, k, gridSizeArg, vertGridArg, vertGridSpacing, strDiagnostic
+    prefix = "gl"+str(k)
             
-                args = []
-                args.append("--horiz-grid-size=" +gridSizeArg)
-                args.append("--vert-grid-size=" +vertGridArg)
-                args.append("--vert-grid-spacing=" +vertGridSpacing)
-                args.append("--prefix="+prefix)
-                args.append("--diagnostic-type="+diagnosticScheme)
+    args = []
+    args.append("--horiz-grid-size=" +gridSizeArg)
+    args.append("--vert-grid-size=" +vertGridArg)
+    args.append("--vert-grid-spacing=" +vertGridSpacing)
+    args.append("--prefix="+prefix)
+    args.append("--diagnostic-type="+diagnosticScheme)
 
-                verifyCommand = sys.executable + " verify.py -abc " + " ".join(args) + " >>refinement-log.txt"
+    verifyCommand = sys.executable + " verify.py -abc " + " ".join(args) + " >>refinement-log.txt"
 
-                caption = gridSizeArg + "x" + gridSizeArg + "x" + vertGridArg + ", " + vertGridSpacing + " spacing, " + strDiagnostic
+    caption = gridSizeArg + "x" + gridSizeArg + "x" + vertGridArg + ", " + vertGridSpacing + " spacing, " + strDiagnostic
 
-                visualsCommand = sys.executable + " createVisuals.py -abc --prefix=" + prefix + " '--subtitle=" + caption + "'"
+    visualsCommand = sys.executable + " createVisuals.py -abc --prefix=" + prefix + " '--subtitle=" + caption + "'"
 
-                print "RUNNING:",verifyCommand
-                os.system(verifyCommand)
-                print "RUNNING:",visualsCommand
-                os.system(visualsCommand)            
-                k += 1
+    print "RUNNING:",verifyCommand
+    os.system(verifyCommand)
+    print "RUNNING:",visualsCommand
+    os.system(visualsCommand)            
+    k += 1
+	
+
+def fullCombinatorics():
+    for gridSizeArg in ["20","40","80"]:
+        for vertGridArg in ["20","40","80"]:
+            for vertGridSpacing in ["glimmer","pattyn","even"]:
+                for diagnosticScheme in ["1","2"]:
+                    runExperiment(gridSizeArg, vertGridArg, vertGridSpacing, diagnosticScheme)
+
+def isolatedVariables():
+    #First do all defaults
+    runExperiment()
+    #Try varying horizontal grid spacing
+    runExperiment(gridSizeArg=20)
+    runExperiment(gridSizeArg=80)
+
+    #Try varying vertical grid spacing
+    runExperiment(vertGridArg=20)
+    runExperiment(vertGridArg=80)
+
+    #Try varying both
+    runExperiment(gridSizeArg=20, vertGridArg=20)
+    runExperiment(gridSizeArg=80, vertGridArg=80)
+
+    #Try different grid spacings
+    runExperiment(vertGridSpacing = "pattyn")
+    runExperiment(vertGridSpacing = "glimmer")
+
+    #Try a colocated grid
+    runExperiment(diagnosticScheme = 1)
 
 guideFile.close()
-            
+
+#Uncomment to run the fully combinatoric version of the experiment
+#fullCombinatorics()
+
+isolatedVariables()
