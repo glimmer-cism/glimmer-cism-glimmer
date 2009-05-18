61f16ccde4b3674e543cedf47c1ac976eb3fd292
Tim Wylie <twylie@users.berlios.de>
2009-05-18 17:09:20 +0000

    New functions added for Pattyn's scheme.  New marinlim case.
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer/branches/lanl@1059 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- src/fortran/glide_ground.F90
+++ src/fortran/glide_ground.F90
@@ -39,7 +39,7 @@ contains
 !-------------------------------------------------------------------------
 
   subroutine glide_marinlim(which,thck,relx,topg,flwa,levels,mask,mlimit,calving_fraction,eus,ablation_field,backstress, & 
-                 tempanmly,dew,dns,backstressmap,sigmabout,sigmabin)
+                 tempanmly,dew,dns,backstressmap,sigmabout,sigmabin,ground,nsn,ewn,usrf)
 
 
     !*FD Removes non-grounded ice, according to one of two altenative
@@ -86,11 +86,14 @@ contains
     real(sp), dimension(:,:), intent(inout) :: backstress
     real(sp) :: tempanmly
     real(dp), intent(in) ::  dew,dns
+    integer, intent(in) ::  nsn,ewn
+    real(dp),dimension(:,:),intent(in)    :: usrf    !*FD usrf
     logical, dimension(:,:), intent(in)   :: backstressmap !*FD map of the
                                                            !*FD backstresses for the initial map
     integer ew,ns
     real(dp) :: sigmab
     real(sp) :: sigmabout,sigmabin
+    type(glide_grnd) :: ground        !*FD ground instance
     !---------------------------------------------------------------------
 
     sigmab = 0.95
@@ -132,56 +135,64 @@ contains
     case(5) ! Relation based on computing the horizontal stretching
             ! of the unconfined ice shelf (\dot \epsilon_{xx}) and multiplying by H.
             ! 
-    do ns = 2, size(backstress,2)-1
-       do ew = 2, size(backstress,1)-1
-            if(.not. backstressmap(ew,ns)) then
-               if (tempanmly > 0.0) then
-                  backstress(ew,ns) = sigmabout
-               else
-                  !backstress(ew,ns) = 1-exp(tempanmly) 
-                  backstress(ew,ns) = sigmabout + (1-sigmabout)*abs(tempanmly/9.2)
-               end if
-            end if
-       end do
-    end do 
-    where (backstressmap) 
-      backstress = sigmabin + (1-sigmabin)*abs(tempanmly/9.2)
-    end where 
-       
-       
-       do ns = 2,size(thck,2)-1
-          do ew = 2,size(thck,1)-1
-             if (GLIDE_IS_GROUNDING_LINE(mask(ew,ns))) then
-                call vertint_output2d(flwa(:,ew-1:ew,ns-1:ns),A, levels * thck(ew,ns))
-                ablation_field(ew,ns)= ((dew*dns)/(50.0d3)**2)* theta * A(2,2) * (sigmaxx * &
-                thck(ew,ns)  * (1 - backstress(ew,ns))) ** gn
-                if ((thck(ew,ns) - ablation_field(ew,ns)) >= 0.0) then
-                  thck(ew,ns) = thck(ew,ns) - ablation_field(ew,ns) 
-                else 
-                  thck(ew,ns) = 0.0d0
+        do ns = 2, size(backstress,2)-1
+           do ew = 2, size(backstress,1)-1
+                if(.not. backstressmap(ew,ns)) then
+                   if (tempanmly > 0.0) then
+                      backstress(ew,ns) = sigmabout
+                   else
+                      !backstress(ew,ns) = 1-exp(tempanmly) 
+                      backstress(ew,ns) = sigmabout + (1-sigmabout)*abs(tempanmly/9.2)
+                   end if
                 end if
-            end if
-          end do
-       end do
-      
-      where (GLIDE_IS_FLOAT(mask).and.relx<mlimit+eus)
-          ablation_field=thck
-          thck = 0.0d0
-       end where
-      do ns = 2,size(thck,2)-1
-         do ew = 2,size(thck,1)-1
-           if (GLIDE_IS_FLOAT(mask(ew,ns)) .and. .not. backstressmap(ew,ns))then 
-      
-             if ((.not. GLIDE_IS_GROUNDING_LINE(mask(ew-1,ns))) &
-              .and. (.not. GLIDE_IS_GROUNDING_LINE(mask(ew+1,ns))) .and. &
-              (.not. GLIDE_IS_GROUNDING_LINE(mask(ew,ns-1))) .and. &
-              (.not. GLIDE_IS_GROUNDING_LINE(mask(ew,ns+1)))) then
-                   thck(ew,ns) = 0.0
-                   ablation_field = thck
-             end if
-           end if
-         end do
-       end do
+           end do
+        end do 
+        where (backstressmap) 
+          backstress = sigmabin + (1-sigmabin)*abs(tempanmly/9.2)
+        end where 
+           
+           
+           do ns = 2,size(thck,2)-1
+              do ew = 2,size(thck,1)-1
+                 if (GLIDE_IS_GROUNDING_LINE(mask(ew,ns))) then
+                    call vertint_output2d(flwa(:,ew-1:ew,ns-1:ns),A, levels * thck(ew,ns))
+                    ablation_field(ew,ns)= ((dew*dns)/(50.0d3)**2)* theta * A(2,2) * (sigmaxx * &
+                    thck(ew,ns)  * (1 - backstress(ew,ns))) ** gn
+                    if ((thck(ew,ns) - ablation_field(ew,ns)) >= 0.0) then
+                      thck(ew,ns) = thck(ew,ns) - ablation_field(ew,ns) 
+                    else 
+                      thck(ew,ns) = 0.0d0
+                    end if
+                end if
+              end do
+           end do
+          
+          where (GLIDE_IS_FLOAT(mask).and.relx<mlimit+eus)
+              ablation_field=thck
+              thck = 0.0d0
+           end where
+          do ns = 2,size(thck,2)-1
+             do ew = 2,size(thck,1)-1
+               if (GLIDE_IS_FLOAT(mask(ew,ns)) .and. .not. backstressmap(ew,ns))then 
+          
+                 if ((.not. GLIDE_IS_GROUNDING_LINE(mask(ew-1,ns))) &
+                  .and. (.not. GLIDE_IS_GROUNDING_LINE(mask(ew+1,ns))) .and. &
+                  (.not. GLIDE_IS_GROUNDING_LINE(mask(ew,ns-1))) .and. &
+                  (.not. GLIDE_IS_GROUNDING_LINE(mask(ew,ns+1)))) then
+                       thck(ew,ns) = 0.0
+                       ablation_field = thck
+                 end if
+               end if
+             end do
+           end do
+    case(6)
+    
+        call update_ground_line(ground, topg, thck, eus, dew, dns, ewn, nsn, mask)
+       
+        where (GLIDE_IS_FLOAT(mask))
+        ablation_field=thck
+        thck = 0.0d0
+      end where
     end select
   end subroutine glide_marinlim
   
@@ -189,29 +200,29 @@ contains
   !This function returns the correct grounding line using the data given 
   ! the mask reference point.  dir is specifying 'ew' or 'ns', but can be 
   ! left null if there's only one option.
-  real function get_ground_line(model,ew1,ns1,ew2,ns2)
+  real function get_ground_line(ground,ew1,ns1,ew2,ns2)
      use glide_types
      implicit none
-     type(glide_global_type) :: model        !*FD model instance
+     type(glide_grnd) :: ground       !*FD glide ground instance
      integer ns1,ew1,ns2,ew2,slot_ns,slot_ew !grounding line in ns/ew direction
      real appr_ground,get_ground_line !grounding line
      
      if (ns1 .eq. ns2) then
          slot_ew = min(ew1,ew2)
-         appr_ground = model%ground%gl_ns(slot_ew,ns1)
+         appr_ground = ground%gl_ns(slot_ew,ns1)
      else if (ew1 .eq. ew2) then
          slot_ns = min(ns1,ns2)
-         appr_ground = model%ground%gl_ew(ew1,slot_ns)
+         appr_ground = ground%gl_ew(ew1,slot_ns)
      end if
      get_ground_line = appr_ground
      return
   end function get_ground_line
     
 !-------------------------------------------------------------------------
-  subroutine set_ground_line(model,ew1,ns1,ew2,ns2,value)
+  subroutine set_ground_line(ground,ew1,ns1,ew2,ns2,value)
      use glide_types
      implicit none
-     type(glide_global_type) :: model        !*FD model instance
+     type(glide_grnd) :: ground        !*FD model instance
      integer, intent(in) :: ns1 !grounding line in ns direction
      integer, intent(in) :: ew1 !grounding line in ew direction
      integer, intent(in) :: ns2 !grounding line in ns direction
@@ -221,17 +232,21 @@ contains
      
      if (ns1 .eq. ns2) then
          slot_ew = min(ew1,ew2)
-         model%ground%gl_ew(slot_ew,ns1) = value
+         ground%gl_ew(slot_ew,ns1) = value
      else if (ew1 .eq. ew2) then
          slot_ns = min(ns1,ns2)
-         model%ground%gl_ns(ew1,slot_ns) = value
+         ground%gl_ns(ew1,slot_ns) = value
      end if
   end subroutine set_ground_line
 !-------------------------------------------------------------------------
-  real function lin_reg_xg(model,ew,ns,j1ew,j1ns)
+  !does the pattyn interpolation for the grounding line
+  real function lin_reg_xg(topg, thck, eus, dew, dns, ew, ns, j1ew, j1ns)
      use glide_types
      use glimmer_physcon, only : rhoi, rhoo
-     type(glide_global_type) :: model        !*FD model instance
+     real(dp),dimension(:,:),intent(in)    :: topg    !*FD Present bedrock topography (scaled)
+     real(dp),dimension(:,:),intent(in)    :: thck    !*FD Present thickness (scaled)
+     real, intent(in) :: eus                       !*FD eustatic sea level
+     real(dp), intent(in) ::  dew, dns
      integer, intent(in) :: ns !grounding line in ns direction
      integer, intent(in) :: ew !grounding line in ew direction
      integer, intent(in) :: j1ns !ice shelf in ns direction
@@ -243,72 +258,84 @@ contains
      real(sp) :: fj_1                      !f evaluated at j (+/-) 1
      real(sp) :: df                        !delta f of fj,jf_1
      real lin_reg_xg
+     
      if (ew .eq. j1ew) then
-        dx = model%numerics%dns
+        dx = dns 
         xj = ns*dx
      else
-        dx = model%numerics%dew
+        dx = dew
         xj = ew*dx
      end if
-     !set the pattyn f function - assuming ocean water
-     fj = (model%climate%eus - model%geometry%topg(ew,ns))*rhoo/(rhoi*model%geometry%thck(ew,ns))
-     fj_1 = (model%climate%eus - model%geometry%topg(j1ew,j1ns))*rhoo/(rhoi*model%geometry%thck(j1ew,j1ns))
-     df = (fj_1 - fj)/dx                
-     xg = (1 - fj + df*xj)/df
+     !set the pattyn f function - assuming ocean water 
+     fj = (eus - topg(ew,ns))*rhoo/(rhoi*thck(ew,ns))
+     if (thck(j1ew,j1ns) .gt. 0.0) then
+         fj_1 = (eus - topg(j1ew,j1ns))*rhoo/(rhoi*thck(j1ew,j1ns))
+         df = (fj_1 - fj)/dx
+         xg = (1 - fj + df*xj)/df
+     else
+         xg = xj
+     end if
+     
      lin_reg_xg = xg
      return 
   end function lin_reg_xg
 !-------------------------------------------------------------------------
-  !also need an update mask
-  subroutine update_ground_line(model,mask)
+  !Loops through the mask and does the interpolation for all the grounding lines
+  subroutine update_ground_line(ground, topg, thck, eus, dew, dns, ewn, nsn, mask)
      use glide_mask
      implicit none
-     type(glide_global_type) :: model        !*FD model instance
+     type(glide_grnd) :: ground        !*FD ground instance
+     real(dp),dimension(:,:),intent(in)    :: topg    !*FD Present bedrock topography (scaled)
+     real(dp),dimension(:,:),intent(in)    :: thck    !*FD Present thickness (scaled)
+     real, intent(in) :: eus                       !*FD eustatic sea level
+     real(dp), intent(in) ::  dew, dns
+     integer, intent(in) ::  ewn, nsn
      integer, dimension(:,:),pointer :: mask    !*FD grid type mask
      integer ew,ns,jns,jew,j1ns,j1ew
      real(sp) :: xg                        !grounding line
      !this is assuming the grounding line is the last grounded pt on the mask
      !reset grounding line data to zero
-     model%ground%gl_ew = 0.0
-     model%ground%gl_ns = 0.0
-     do ns = 1,model%general%nsn
-        do ew = 1,model%general%ewn
+     ground%gl_ew = 0.0
+     ground%gl_ns = 0.0
+     do ns = 1,nsn
+        do ew = 1,ewn
             if (GLIDE_IS_GROUNDING_LINE(mask(ew,ns))) then
                 !the grounding line always rounds down so it is grounded.
-                !northern grounding line
+                !southern grounding line
                 if (GLIDE_IS_OCEAN(mask(ew,ns - 1)) &
                         .or. (GLIDE_IS_FLOAT(mask(ew,ns - 1)))) then
-                    xg = lin_reg_xg(model,ew,ns,ew,ns-1)
-                    call set_ground_line(model,ew,ns,ew,ns-1,xg)
-                !southern grounding line
+                    xg = lin_reg_xg(topg,thck,eus,dew,dns,ew,ns,ew,ns-1)
+                    call set_ground_line(ground,ew,ns,ew,ns-1,xg)
+                !northern grounding line
                 else if (GLIDE_IS_OCEAN(mask(ew,ns + 1)) &
                         .or. (GLIDE_IS_FLOAT(mask(ew,ns + 1)))) then
-                    xg = lin_reg_xg(model,ew,ns,ew,ns+1)
-                    call set_ground_line(model,ew,ns,ew,ns+1,xg) 
+                    xg = lin_reg_xg(topg,thck,eus,dew,dns,ew,ns,ew,ns+1)
+                    call set_ground_line(ground,ew,ns,ew,ns+1,xg) 
                 end if 
                 
                 !western grounding line
                 if (GLIDE_IS_OCEAN(mask(ew - 1,ns)) &
                         .or. GLIDE_IS_FLOAT(mask(ew - 1,ns))) then
-                    xg = lin_reg_xg(model,ew,ns,ew - 1,ns)
-                    call set_ground_line(model,ew,ns,ew-1,ns,xg)
+                    xg = lin_reg_xg(topg,thck,eus,dew,dns,ew,ns,ew - 1,ns)
+                    call set_ground_line(ground,ew,ns,ew-1,ns,xg)
                 !eastern grounding line
                 else if (GLIDE_IS_OCEAN(mask(ew + 1,ns)) &
                         .or. GLIDE_IS_FLOAT(mask(ew + 1,ns))) then
-                    xg = lin_reg_xg(model,ew,ns,ew + 1,ns)
-                    call set_ground_line(model,ew,ns,ew + 1,ns,xg)
-                !need to update mask
+                    xg = lin_reg_xg(topg,thck,eus,dew,dns,ew,ns,ew + 1,ns)
+                    call set_ground_line(ground,ew,ns,ew + 1,ns,xg)
                 end if
             end if 
         end do
      end do
   end subroutine update_ground_line
   !-------------------------------------------------------------------------
-  !also need an update mask
-  real function get_ground_thck(model,ew1,ns1,ew2,ns2)
+  real function get_ground_thck(ground,topg,usrf,dew,dns,ew1,ns1,ew2,ns2)
      use glide_types
      implicit none
-     type(glide_global_type) :: model        !*FD model instance
+     type(glide_grnd) :: ground        !*FD ground instance
+     real(dp),dimension(:,:),intent(in)    :: topg    !*FD Present bedrock topography (scaled)
+     real(dp),dimension(:,:),intent(in)    :: usrf    !*FD surface height
+     real(dp), intent(in) ::  dew, dns
      integer ns1,ew1,ns2,ew2,min_ns,min_ew,max_ns,max_ew !grounding line in ns/ew direction
      real(sp) ::  xg                        !grounding line
      real(sp) ::  tg                        !topographic height at grounding line
@@ -325,25 +352,25 @@ contains
          max_ew = max(ew1,ew2)
          min_ns = ns1
          max_ns = ns1
-         x0 = min_ew*model%numerics%dew
-         x1 = max_ew*model%numerics%dew
+         x0 = min_ew*dew !model%numerics%dew
+         x1 = max_ew*dew
      else if (ew1 .eq. ew2) then
          min_ns = min(ns1,ns2)
          max_ns = max(ns1,ns2)
          min_ew = ew1
          max_ew = ew1
-         x0 = min_ns*model%numerics%dns
-         x1 = max_ns*model%numerics%dns
+         x0 = min_ns*dns !model%numerics%dns
+         x1 = max_ns*dns
      end if
      !get grounding line
-     xg = model%ground%gl_ns(min_ew,min_ns)
+     xg = ground%gl_ns(min_ew,min_ns)
      !find top height at xg
-     y0 = model%geometry%topg(min_ew,min_ns)
-     y1 = model%geometry%topg(max_ew,max_ns)
+     y0 = topg(min_ew,min_ns) !model%geometry%topg
+     y1 = topg(max_ew,max_ns)
      tg = y0 + (xg - x0)*((y1 - y0)/(x1 - x0))
      !find ice at xg
-     y0 = model%geometry%usrf(min_ew,min_ns)
-     y1 = model%geometry%usrf(max_ew,max_ns)
+     y0 = usrf(min_ew,min_ns) !model%geometry%usrf
+     y1 = usrf(max_ew,max_ns)
      ig = y0 + (xg - x0)*((y1 - y0)/(x1 - x0))
      !thickness
      hg = ig - tg
